7a26a3114f970c74abf708f90ff9e5b6a87c1905
mfekadu <mike.fekadu@gmail.com>
2019-09-01 23:51:56 -0400

    iterating over the files with fancy IO functions to intelligently handle bad lines from dataset

---
+++ RedirectStdStreams.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import sys
+
+# https://stackoverflow.com/a/32353549/5411712
+# helpful for accessing the information in pandas stdout/stderr 
+class RedirectStdStreams(object):
+    def __init__(self, stdout=None, stderr=None):
+        self._stdout = stdout or sys.stdout
+        self._stderr = stderr or sys.stderr
+
+    def __enter__(self):
+        self.old_stdout, self.old_stderr = sys.stdout, sys.stderr
+        self.old_stdout.flush(); self.old_stderr.flush()
+        sys.stdout, sys.stderr = self._stdout, self._stderr
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._stdout.flush(); self._stderr.flush()
+        sys.stdout = self.old_stdout
+        sys.stderr = self.old_stderr
+
+
+if __name__ == "__main__":
+    from io import StringIO
+    s = StringIO("some string buffer")
+    print(s.read())
+    s.seek(0)
+    # demonstrate Redirect 
+    # by writing "hello" to stdout via print
+    # should overwrite "some s" with "hello\t" but keep rest of buffer
+    with RedirectStdStreams(stdout=s):
+        print("hello",end='\t')
+    s.seek(0)
+    print(s.read())
--- convert_to_csai.py
+++ convert_to_csai.py
@@ -2,6 +2,9 @@
 
 from os import listdir, getcwd
 from os.path import join
+from RedirectStdStreams import RedirectStdStreams
+from io import StringIO
+import pandas as pd
 
 DATA_DIR = 'STACIA_QA'
 DIR = join(getcwd(), DATA_DIR)
@@ -17,14 +20,28 @@ stat_files = [f for f in filter(startswith('stat'), files)]
 club_files = [f for f in filter(startswith('club'), files)]
 
 # encodings: https://stackoverflow.com/q/19699367/5411712
+# list-of-encodings: 
+# https://docs.python.org/3/library/codecs.html#standard-encodings
 for fname in cs_files:
-    with open(join(DIR, fname), 'r', encoding='ISO-8859-1') as f:
-        print(f.read())
+    warnings = StringIO() # to catch the warnings
+    with RedirectStdStreams(stderr=warnings):
+        pd.read_csv(join(DIR, fname), 
+                    sep='|',
+                    encoding='latin_1',
+                    error_bad_lines=False,
+                    warn_bad_lines=True)
+    warnings.seek(0)
+    print("warnings??",warnings.read())
+    print(fname,"done","....on to the next one....")
 
 for fname in stat_files:
-    with open(join(DIR, fname), 'r', encoding='ISO-8859-1') as f:
-        print(f.read())
+    pd.read_csv(join(DIR, fname), 
+                sep='|', 
+                encoding='latin_1',
+                error_bad_lines=False)
 
 for fname in club_files:
-    with open(join(DIR, fname), 'r', encoding='ISO-8859-1') as f:
-        print(f.read())
+    pd.read_csv(join(DIR, fname), 
+                sep='|', 
+                encoding='latin_1',
+                error_bad_lines=False)
