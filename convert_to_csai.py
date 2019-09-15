2c9767ce6b867045d4249ab77bb011af5c30ae78
mfekadu <mike.fekadu@gmail.com>
2019-09-15 13:39:29 -0400

    create report.html with pandas_profiling
    to reduce the size of the ipynb... also downloading as a .py file is an option.. that's cool.

--- convert_to_csai.py
+++ convert_to_csai.py
@@ -1,88 +1,174 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Conversion
+# There is Question/Answer (QA) data inside of `STACIA_QA`.
+# 
+# The goal of this notebook is to read all of the csv files into a `pandas.DataFrame`
+# 
+# Then reformat the data to fit the CSAI style.
+# 
+# Finally, output a CSV file of the newly structured data. 
+
+# In[1]:
+
 
 from os import listdir, getcwd
 from os.path import join
 from RedirectStdStreams import RedirectStdStreams
 from io import StringIO
 import pandas as pd
+import re
+import numpy as np
+import pandas_profiling
+
+
+# In[2]:
+
 
 DATA_DIR = 'STACIA_QA'
 DIR = join(getcwd(), DATA_DIR)
 
+
+# In[3]:
+
+
 files = listdir(DATA_DIR)
+
+
+# In[4]:
 
 
 def startswith(y):
     return lambda x: x.startswith(y)
 
 
-cs_files = [f for f in filter(startswith('cs'), files)]
-stat_files = [f for f in filter(startswith('stat'), files)]
-club_files = [f for f in filter(startswith('club'), files)]
+# In[5]:
 
-# encodings: https://stackoverflow.com/q/19699367/5411712
-# list-of-encodings:
-# https://docs.python.org/3/library/codecs.html#standard-encodings
-for fname in cs_files:
-    fpath = join(DIR, fname)
-    warnings = StringIO()  # to catch the warnings
-    with RedirectStdStreams(stderr=warnings):
-        pd.read_csv(fpath,
-                    sep='|',
-                    encoding='latin_1',
-                    header=None,
-                    error_bad_lines=False,
-                    warn_bad_lines=True)
-    warnings.seek(0)
-    warns = warnings.read()
-    warnings.close()
 
-    if len(warns) > 10:
-        # for whatever reason the warnings look like "b'Skipping...'\n"
-        # so chop off the weird stuff on the edges
-        warns = warns[2:len(warns)-2]
-        warns = warns.replace('\\n', '\n')
-        with StringIO(warns) as w:
-            warn_df = pd.read_csv(w, sep='\n', header=None)
-            warn_df.columns = ['warns']
-            # https://chrisalbon.com/python/data_wrangling/pandas_regex_to_create_columns/
-            warn_df['skipped_lines'] = warn_df['warns'].str.extract(
-                'Skipping line (\d+)', expand=True)
-            warn_df['expected_cols'] = warn_df['warns'].str.extract(
-                'expected (\d+)', expand=True)
-            warn_df['actual_cols'] = warn_df['warns'].str.extract(
-                'saw (\d+)', expand=True)
+cs_files = sorted(filter(startswith('cs'), files))
+stat_files = sorted(filter(startswith('stat'), files))
+club_files = sorted(filter(startswith('club'), files))
 
-            with open(fpath, encoding='latin_1') as f:
-                lines = f.readlines()
-                line_num = int(warn_df['skipped_lines'][0])
-                bad_line = lines[line_num - 1]  # because lines are 1-indexed
-                print('bad_line is #', line_num, '...', bad_line)
 
-    print(fname, "done", "....on to the next one....")
+# In[6]:
 
-for fname in stat_files:
-    warnings = StringIO()  # to catch the warnings
-    with RedirectStdStreams(stderr=warnings):
-        pd.read_csv(join(DIR, fname),
-                    sep='|',
-                    encoding='latin_1',
-                    header=None,
-                    error_bad_lines=False,
-                    warn_bad_lines=True)
-    warnings.seek(0)
-#    print("warnings??",warnings.read())
-    print(fname, "done", "....on to the next one....")
 
-for fname in club_files:
-    warnings = StringIO()  # to catch the warnings
-    with RedirectStdStreams(stderr=warnings):
-        pd.read_csv(join(DIR, fname),
-                    sep='|',
-                    encoding='latin_1',
-                    header=None,
-                    error_bad_lines=False,
-                    warn_bad_lines=True)
-    warnings.seek(0)
-#    print("warnings??",warnings.read())
-    print(fname, "done", "....on to the next one....")
+def make_data_frame_from_files(files):
+    '''given stacia_qa csv files, return a pandas DataFrame containing the enclosed data'''
+    # encodings: https://stackoverflow.com/q/19699367/5411712
+    # list-of-encodings:
+    # https://docs.python.org/3/library/codecs.html#standard-encodings
+    df = pd.DataFrame()
+    for fname in files:
+        fpath = join(DIR, fname)
+        to_concat = [df]
+        to_concat.append(
+            pd.read_csv(fpath,
+                        sep='|',
+                        encoding='latin_1',
+                        header=None,
+                        # fun fact: setting the `names` param avoids warnings
+                        # because then pandas knows how many columns to make
+                        # also the purpose of the "other" columns is to catch weird 
+                        # extras that would otherwise be trimmed off after the delimeter
+                        names=['id','q_format','a_format','notes','other','other2','other3'],
+                        error_bad_lines=False,
+                        warn_bad_lines=True))
+        df = pd.concat(to_concat)
+        print(fname, "done", "....on to the next one....")
+    return df
+
+
+# In[7]:
+
+
+cs_df   = make_data_frame_from_files(cs_files)
+stat_df = make_data_frame_from_files(stat_files)
+club_df = make_data_frame_from_files(club_files)
+
+
+# In[8]:
+
+
+# remove the comments at the end of each file 
+cs_df = cs_df[cs_df['id'].str.startswith("[") != True]
+stat_df = stat_df[stat_df['id'].str.startswith("[") != True]
+club_df = club_df[club_df['id'].str.startswith("[") != True]
+
+all_df = pd.concat([cs_df, stat_df, club_df])
+
+
+# ## Let's have a quick look at `all_df`
+
+# In[9]:
+
+
+print('all_df.shape:', all_df.shape)
+print('So there are {} question/answer pairs'.format(all_df.shape[0]))
+all_df.tail()
+
+
+# ## Is there bad data?  `yes` 😢
+
+# In[10]:
+
+
+def make_weird_columns_mask(df):
+    '''given a pandas DataFrame of STACIA_QA data, return a mask to find the rows with extra columns'''
+    wierd_columns_mask = df['notes'].notnull() 
+    wierd_columns_mask |= df['other'].notnull() 
+    wierd_columns_mask |= df['other2'].notnull() 
+    wierd_columns_mask |= df['other3'].notnull()
+    return wierd_columns_mask
+
+
+# In[11]:
+
+
+wierd_columns_mask = make_weird_columns_mask(all_df)
+needs_to_be_fixed = all_df[wierd_columns_mask]
+print('needs_to_be_fixed.shape:', needs_to_be_fixed.shape)
+print('So there are {} rows that do not follow the standard STACIA_QA format'.format(needs_to_be_fixed.shape[0]))
+
+
+# In[12]:
+
+
+all_df[wierd_columns_mask].head()
+
+
+# In[13]:
+
+
+all_df[wierd_columns_mask].tail()
+
+
+# ## Frankly, `84` bad rows is so few that it might be reasonable to drop those rows
+
+# In[14]:
+
+
+all_df[wierd_columns_mask != True][['id','q_format','a_format']].head()
+
+
+# In[15]:
+
+
+all_df[wierd_columns_mask != True][['id','q_format','a_format']].tail()
+
+
+# ## Finally, some reasonable looking data!
+# ### To be continued...
+
+# In[16]:
+
+
+pandas_profiling.ProfileReport(all_df).to_file('report.html')
+
+
+# In[ ]:
+
+
+
+
